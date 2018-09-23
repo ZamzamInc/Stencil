b319f48138113c2b2ce4efc8fdf1099cda687528
David Jennes <david.jennes@gmail.com>
2018-09-23 04:21:01 +0200

    SPM

--- Package.swift
+++ Package.swift
@@ -4,19 +4,19 @@ import PackageDescription
 let package = Package(
   name: "Stencil",
   products: [
-    .library(name: "Stencil", targets: ["Stencil"]),
+    .library(name: "Stencil", targets: ["Stencil"])
   ],
   dependencies: [
     .package(url: "https://github.com/kylef/PathKit.git", from: "0.9.0"),
-    .package(url: "https://github.com/kylef/Spectre.git", from: "0.9.0"),
+    .package(url: "https://github.com/kylef/Spectre.git", from: "0.9.0")
   ],
   targets: [
     .target(name: "Stencil", dependencies: [
-      "PathKit",
+      "PathKit"
     ], path: "Sources"),
     .testTarget(name: "StencilTests", dependencies: [
       "Stencil",
-      "Spectre",
+      "Spectre"
     ])
   ]
 )
